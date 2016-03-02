e7f56b31473cfcb9ae86f8588d539c9b839445f1
Luis-Lamat <beto_alaska@hotmail.com>
2016-03-01 22:58:56 -0600

    Ball moves correctly, bounces correctly, missing game mechanics

--- VistaOrtogonalglOrtho/main.cpp
+++ VistaOrtogonalglOrtho/main.cpp
@@ -32,22 +32,31 @@ float paddleWidth = 1, paddleHeight = 2.0, paddleDepth = 0.1;
 float ballRadius = 0.5, ballWidth = 1.0, ballHeight = 1.0, ballDepth = 1.0;
 
 // left paddle position
-float leftPaddleX = -3.5, leftPaddleY = 0.0;
+float leftPaddleX = -3.5, leftPaddleY = 0.0, leftPaddleFaceX = -3;
 float leftPaddleTopY = 1.0, leftPaddleBotY = -1.0;
 
 // right paddle postiion
-float rightPaddleX = 3.5, rightPaddleY = 0.0;
+float rightPaddleX = 3.5, rightPaddleY = 0.0, rightPaddleFaceX = 3;
 float rightPaddleTopY = 1.0, rightPaddleBotY = -1.0;
 
-// ball position
-float ballX = 0.0, ballY = 0.0;
+// ball position and direction
+float ballX = 1.0, ballY = 0.0;
+int ballDirection = 1; // a number for each cartesian quadrant
+// row 0 is for x coord. direction changes and row 1 for y-coord. changes
+int directionVector[2][5] = {{0, 1, -1, -1, 1}, {0, 1, 1, -1, -1}};
+// row 0 is for x-changes (y collision) and row 1 for y-changes (x-collision)
+int directionChange[2][5] = {{0, 2,  1,  4, 3}, {0, 4, 3,  2,  1}};
 
 // Frustum value ranges
 const float X_MAX = 4.0, X_MIN = -4.0;
 const float Y_MAX = 4.0, Y_MIN = -4.0;
 
 // movement
-float paddleSpeed = 0.5;
+float paddleSpeed = 0.5, ballSpeed = 0.1;
+
+// game state definition
+typedef enum { PLAYING, PAUSED, GAME_OVER } State;
+State gameState = PAUSED;
 
 
 /* ------------------------------- Functions -------------------------------- */
@@ -68,8 +77,33 @@ void updateRightPaddleLoc (float delta) {
     }
 }
 
+void updateBallLoc (float delta){
+    // checking for left or right paddle collisions
+    if ((ballY <= leftPaddleTopY && ballY >= leftPaddleBotY &&
+         ballX - ballRadius <= leftPaddleFaceX) ||
+        (ballY <= rightPaddleTopY && ballY >= rightPaddleBotY &&
+         ballX + ballRadius >= rightPaddleFaceX)) {
+        printf("Paddle Collision!!!\n");
+        ballDirection = directionChange[0][ballDirection];
+    } else if ((ballX + ballRadius >= X_MAX) || (ballX - ballRadius <= X_MIN)) {
+        printf("Left / Right Collision!!!\n");
+        ballDirection = directionChange[0][ballDirection];
+    } else if ((ballY + ballRadius >= Y_MAX) || (ballY - ballRadius <= Y_MIN)) {
+        printf("Top / Down Collision!!!\n");
+        ballDirection = directionChange[1][ballDirection];
+    }
+
+    // updating the paddle's position
+    ballX += (delta * directionVector[0][ballDirection]);
+    ballY += (delta * directionVector[1][ballDirection]);
+}
+
+void resetBallLoc() {
+    ballX = 0.0; ballY = 0.0;
+}
+
 void startGame (int startPosition) {
-    
+    gameState = PLAYING;
 }
 
 void drawPaddles () {
@@ -171,6 +205,16 @@ void keyboardPressed (unsigned char key, int mouseX, int mouseY) {
     }
 }
 
+void gameTimer (int value){
+    if (gameState == PLAYING) {
+        updateBallLoc(ballSpeed);
+    } else if (gameState == GAME_OVER) {
+        resetBallLoc();
+    }
+    glutPostRedisplay();
+    glutTimerFunc(100, gameTimer, 1);
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     
@@ -187,8 +231,8 @@ void reshape(int w, int h) {
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    // glOrtho(-4.0, 4.0, -4.0, 4.0, 1.0, 4.0);
-    glFrustum(X_MIN, X_MAX, Y_MIN, Y_MAX, 1.0, 4.0);
+    glOrtho(-4.0, 4.0, -4.0, 4.0, 1.0, 4.0);
+    // glFrustum(X_MIN, X_MAX, Y_MIN, Y_MAX, 1.0, 4.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(0, 0, 1.1, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
@@ -210,7 +254,7 @@ int main(int argc, char** argv) {
     glutKeyboardFunc(keyboardPressed);
     glutSpecialFunc(arrowKeysPressed);
     init();
-    //glutTimerFunc(1000, myTimer,   1);
+    glutTimerFunc(100, gameTimer, 1);
     glutMainLoop();
 }
 
